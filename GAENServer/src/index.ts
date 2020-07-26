262585a406fb8e4785626c1799df00492f4a269f
SeungHyun Lee <xion@kaist.ac.kr>
2020-07-27 08:41:49 +0900

    define entities

--- GAENServer/src/index.ts
+++ GAENServer/src/index.ts
@@ -1,21 +1,35 @@
 import "reflect-metadata";
 import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+import {Log} from "./entity/Log";
+import {Config} from "./entity/Config"
 
+
+console.log("Connecting to MySQL DB...");
 createConnection().then(async connection => {
+    console.log("Connected to MySQL DB.");
 
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+    console.log("Initializing config...");
+    
+    const config = new Config();
+    config.version = 0;
+    config.SCAN_PERIOD = 300000;
+    config.SCAN_DURATION = 8000;
+    config.SERVICE_UUID = 'aa';
+    config.advertiseMode = 0;
+    config.advertiseTxPower = 3;
+    config.scanMode = 0;
 
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
+    await connection.manager.getRepository(Config)
+        .createQueryBuilder()
+        .insert()
+        .orIgnore()
+        .into(Config)
+        .values(config)
+        .execute();
+    
+    console.log("Initialized config.");
 
-    console.log("Here you can setup and run express/koa/any other framework.");
+    /// DEBUG ///
+    console.log(await connection.manager.getRepository(Config).find());
 
 }).catch(error => console.log(error));

bb47f30b30400b94ce89bc3e188e7a02f5394a13
totolacky <totolacky@kaist.ac.kr>
2020-08-18 03:35:15 +0900

    Update server url for console

--- GAENConsole/gaen-console/src/main.js
+++ GAENConsole/gaen-console/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import io from 'socket.io-client';
-const socket = io('http://192.249.19.249:4580'); 
+const socket = io('http://192.249.19.252:2880'); 
 
 Vue.prototype.$socket = socket;
 Vue.config.productionTip = false

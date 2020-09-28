36cafe5afd92553a52f9991ec6c76fb00b14b4cc
Xion <xion@kaist.ac.kr>
2020-09-28 14:29:05 +0900

    fix docker/mysql/run.sh

--- docker/mysql/run.sh
+++ docker/mysql/run.sh
@@ -1,7 +1,4 @@
 sudo docker run --name gaen-mysql-server -p 3306:3306 \
-    -v ${PWD}/../../MySQL_Commands.sql:/docker-entrypoint-initdb.d/MySQL_Commands.sql
-    -e MYSQL_USER=GAEN \
-    -e MYSQL_PASSWORD=GAENtest \
-    -e MYSQL_DATABASE=gaen_db \
+    -v ${PWD}/../../GAENServer/MySQL_Commands.sql:/docker-entrypoint-initdb.d/MySQL_Commands.sql \
     -d mysql/mysql-server:latest \
     --default-authentication-plugin=mysql_native_password

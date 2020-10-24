f9c480681049910ca5c600e731708fc1b032812b
Xion <xion@kaist.ac.kr>
2020-10-25 02:54:22 +0900

    sync sql schema

--- GAENServer/MySQL_Commands.sql
+++ GAENServer/MySQL_Commands.sql
@@ -2,8 +2,8 @@
 CREATE DATABASE gaen_db;
 
 -- Create new user
-CREATE USER 'GAEN'@'localhost' IDENTIFIED BY 'GAENtest';
-GRANT ALL PRIVILEGES ON gaen_db.* TO 'GAEN'@'localhost';
+CREATE USER 'GAEN'@'172.0.0.0/255.0.0.0' IDENTIFIED BY 'GAENtest';
+GRANT ALL PRIVILEGES ON gaen_db.* TO 'GAEN'@'172.0.0.0/255.0.0.0';
 
 -- Log in as new user
 
@@ -22,6 +22,13 @@ CREATE TABLE logs (
     attenuation INT,
     UNIQUE KEY (myId, otherId, time, logType)
 );
+CREATE TABLE logs_general (
+    id INT PRIMARY KEY,
+    testId CHAR(100),
+    myId CHAR(36) NOT NULL,
+    time TIMESTAMP(6) NOT NULL,
+    msg CHAR(100)
+);
 CREATE TABLE config (
     version INT NOT NULL PRIMARY KEY,
     SCAN_PERIOD BIGINT NOT NULL,
@@ -31,7 +38,8 @@ CREATE TABLE config (
     SERVICE_UUID INT NOT NULL,
     advertiseMode INT NOT NULL,
     advertiseTxPower INT NOT NULL,
-    scanMode INT NOT NULL
+    scanMode INT NOT NULL,
+    initJitter BOOL NOT NULL
 );
 CREATE TABLE scan_instances (
     id INT PRIMARY KEY,
@@ -39,12 +47,12 @@ CREATE TABLE scan_instances (
     myId CHAR(36) NOT NULL,
     otherId CHAR(36),
     time TIMESTAMP(6) NOT NULL,
-    secondsSinceLastScan INT NOT NULL,
-    typicalAttenuation INT NOT NULL,
-    typicalPowerAttenuation INT NOT NULL,
-    minAttenuation INT NOT NULL
+    secondsSinceLastScan INT,
+    typicalAttenuation INT,
+    typicalPowerAttenuation INT,
+    minAttenuation INT
 );
 
 -- Initialize config
-INSERT IGNORE INTO config (version, SCAN_PERIOD, SCAN_DURATION, MAX_JITTER, UPLOAD_PERIOD, SERVICE_UUID, advertiseMode, advertiseTxPower, scanMode) 
-VALUES (0b01000000, 300000, 8000, 90000, 3600000, 0xfd6f, 1, 1, 2);
+INSERT IGNORE INTO config (version, SCAN_PERIOD, SCAN_DURATION, MAX_JITTER, UPLOAD_PERIOD, SERVICE_UUID, advertiseMode, advertiseTxPower, scanMode, initJitter) VALUES (0b01000000, 300000, 4000, 90000, 3600000, 0xfd6f, 1, 1, 2, TRUE);
+
